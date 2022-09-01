22dd9ddde9a0fc14c01117bced32b777a7199abb
josh-lew <joshlewisdev@gmail.com>
2022-09-01 18:33:46 +0100

    additional files as part of previous commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Splash from './Components/Splash';
+import FinanceInstructions from './Components/FinanceInstructions';
+import FinanceForm from './Components/FinanceForm';
+import FormContainer from './Components/FormContainer';
+import SubmitButton from './Components/SubmitButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <section className="container col-7">
+        <div className='row justify-content-center'>
+          <Splash />
+        </div>
+        <div>
+          <FormContainer />
+        </div>
+        <div>
+          <SubmitButton />
+        </div>
+        
+         
+      </section>
+      </>
   );
 }
 
